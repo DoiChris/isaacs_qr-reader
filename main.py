c81d41437ddcd3157b0c45d6ecc29f33237f92af
DoiChris <pej.deutinger@gmail.com>
2018-08-23 20:39:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def draw_positions(frame, decoded_objs):
         frame = cv2.rectangle(frame,
                               (left, top),
                               (left + width, height + top),
-                              (0, 0, 168), 2)
+                              (0, 0, 168), 5)
 
 
 if __name__ == '__main__':
